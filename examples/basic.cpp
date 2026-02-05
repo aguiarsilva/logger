986037392d53aa1e89d64f39b87dd1c70e8a0f39
Bruno Aguiar <aguiarsilva@gmail.com>
2026-02-05 11:03:59 +0100

    basic example finished

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -4,6 +4,7 @@
 #include "logger/sink.hpp"
 #include "logger/console_sink.hpp"
 #include "logger/file_sink.hpp"
+#include "logger/simple_formatter.hpp"
 #include <memory>
 #include <iostream>
 
@@ -13,6 +14,11 @@ int main() {
   
     Logger log("MyApp");
     log.add_sink(std::make_shared<ConsoleSink> (
+        std::make_unique<SimpleFormatter>()
+    ));
+
+    log.add_sink(std::make_shared<FileSink> (
+        "app.log",
         std::make_unique<SimpleFormatter>()
     ));
 
